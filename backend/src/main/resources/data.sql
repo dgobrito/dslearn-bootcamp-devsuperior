572ddf7de6ff3c416559d9f7b32b9c0f8fa30f9d
Dyego dos Santos Brito <dyego.eti@gmail.com>
2023-02-22 19:29:48 -0300

    Offer

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -16,3 +16,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
  INSERT INTO TB_COURSE (name, img_uri, img_gray_uri) VALUES ('Bootcamp HTML', 'https://cuboup.s3.sa-east-1.amazonaws.com/br/wp-content/uploads/2022/08/14174056/Terraform-Course-Image.png', 'https://get.pxhere.com/photo/learn-online-book-silhouette-icon-concept-study-education-research-library-world-encyclopedia-global-learning-university-web-internet-course-webinar-guide-read-publish-design-logo-symbol-emblem-line-art-clip-art-1620283.jpg');
+ 
+ INSERT INTO TB_OFFER (edition, start_moment, end_moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', 1);
+ INSERT INTO TB_OFFER (edition, start_moment, end_moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1);
